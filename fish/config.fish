93015291ff7fe154f8957bcd33dbe1e2c04777b4
LordMZTE <lord@mzte.de>
2021-04-13 01:06:56 +0200

    fish config stuff

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,11 @@ fish_vi_key_bindings
 alias ls="lsd"
 alias ll="ls -l"
 alias la="ll -a"
-alias lt="la --tree"
+alias lt="ll --tree"
 alias clip="xclip -sel clip"
+alias nv="nvim"
+alias nvide="rbg neovide --multiGrid"
+
 function rbg
     $argv &>/dev/null&
 end
@@ -14,6 +17,12 @@ end
 function rbgd
     rbg $argv
     disown
+end
+
+function foreachdir
+    for file in (find $argv[1] -type f -print)
+        f=$file eval $argv[2..-1]
+    end
 end
 
 function !! 
@@ -25,12 +34,26 @@ function mkdircd
     cd $argv[1]
 end
 
+function ifpresent
+    if which $argv[1] &> /dev/null
+        eval $argv[2..-1]
+    end
+end
+
+function todos
+    rg -i -H todo
+end
+
 # ENV
 export CXX=clang++
 export EDITOR=nvim
 
-export PATH="$PATH:$HOME/.cargo/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin"
+export PATH="$PATH:$HOME/.cargo/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin:$HOME/go/bin"
 
-starship init fish | source
-zoxide init fish | source
-cod init %self fish | source
+# initialization stuff
+ifpresent starship 'starship init fish | source'
+ifpresent zoxide 'zoxide init fish | source'
+ifpresent cod 'cod init %self fish | source'
+ifpresent navi 'navi widget fish | source'
+ifpresent opam 'eval (opam env)'
+
