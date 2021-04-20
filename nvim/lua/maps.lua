d46576ce11cf400137f41dcb6d647d58cdd45bcd
LordMZTE <lord@mzte.de>
2021-04-20 14:43:22 +0200

    config fixes

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,12 +1,12 @@
 local map = vim.api.nvim_set_keymap
 
 -- Getting stuck in ~~vim~~ terminal
-map("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
-map("n", "fzf", ":FZF<CR>", { silent = true, noremap = true })
+map("t", "<Esc>", "<C-\\><C-n>", {})
+map("n", "fzf", ":FZF<CR>", { silent = true })
 
 -- Quick cursor movement
-map("n", "<C-Down>", "5k", { noremap = true })
-map("n", "<C-Up>", "5j", { noremap = true })
+map("n", "<C-Down>", "5j", { noremap = true })
+map("n", "<C-Up>", "5k", { noremap = true })
 
 -- Quick pasting/yoinking to system register
 map("n", "+y", "\"+y", { noremap = true })
@@ -18,24 +18,24 @@ map("n", "*p", "\"*p", { noremap = true })
 map("n", "*d", "\"*d", { noremap = true })
 
 -- symbol renaming
-map("n", "cn", "<Plug>(coc-rename)", { silent = true, noremap = true })
+map("n", "cn", "<Plug>(coc-rename)", { silent = true })
 -- apply AutoFix to problem on current line
-map("n", "cf", "<Plug>(coc-fix-current)", { silent = true, noremap = true })
+map("n", "cf", "<Plug>(coc-fix-current)", { silent = true })
 -- open action dialog
-map("n", "ca", ":CocAction<CR>", { silent = true, noremap = true })
+map("n", "ca", ":CocAction<CR>", { silent = true })
 
 -- GoTo code navigation.
-map("n", "gd", "<Plug>(coc-definition)", { silent = true, noremap = true })
-map("n", "gy", "<Plug>(coc-type-definition)", { silent = true, noremap = true })
-map("n", "gi", "<Plug>(coc-implementation)", { silent = true, noremap = true })
-map("n", "gr", "<Plug>(coc-references)", { silent = true, noremap = true })
+map("n", "gd", "<Plug>(coc-definition)", { silent = true })
+map("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
+map("n", "gi", "<Plug>(coc-implementation)", { silent = true })
+map("n", "gr", "<Plug>(coc-references)", { silent = true })
 
 -- Use K to show documentation in preview window.
-map("n", "K", ":call CocActionAsync(\'doHover\')<CR>", { silent = true, noremap = true })
+map("n", "K", ":call CocActionAsync(\'doHover\')<CR>", { silent = true })
 
 -- use space o to show symbols
-map("n", "<space>o", ":CocList -I symbols<CR>", { silent = true, noremap = true })
+map("n", "<space>o", ":CocList -I symbols<CR>", { silent = true })
 
 -- format code
-map("n", "cr", ":call CocActionAsync(\'format\')<CR>", { silent = true, noremap = true })
+map("n", "cr", ":call CocActionAsync(\'format\')<CR>", { silent = true })
 
