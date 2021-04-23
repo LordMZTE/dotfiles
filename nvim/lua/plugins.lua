e2f64fbf067d3b767fb392bac3cfaafa791ddbe3
LordMZTE <lord@mzte.de>
2021-04-23 21:05:26 +0200

    nvim config changes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,11 +16,12 @@ return require("packer").startup(function(use)
     use "tpope/vim-endwise"
     use "vimwiki/vimwiki"
     use {"glacambre/firenvim", run = function() vim.fn["firenvim#install"](0) end}
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    use 'jreybert/vimagit'
-    use 'airblade/vim-gitgutter'
-    use 'dag/vim-fish'
-    use 'uiiaoo/java-syntax.vim'
+    use "vim-airline/vim-airline"
+    use "vim-airline/vim-airline-themes"
+    use "jreybert/vimagit"
+    use "airblade/vim-gitgutter"
+    use "dag/vim-fish"
+    use "uiiaoo/java-syntax.vim"
+    use "honza/vim-snippets"
 end)
 
