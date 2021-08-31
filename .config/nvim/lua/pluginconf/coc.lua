a33d26d45dfa8ac5151bdbbdbc7a0f4457cb269e
LordMZTE <lord@mzte.de>
2021-08-31 22:24:12 +0200

    update nvim config

--- .config/nvim/lua/pluginconf/coc.lua
+++ .config/nvim/lua/pluginconf/coc.lua
@@ -12,3 +12,22 @@ vim.g.coc_global_extensions = {
     "coc-tabnine",
     "coc-toml",
 }
+
+local function check_back_space()
+  local col = vim.fn.col(".") - 1
+  return col <= 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
+function tab_completion()
+  if vim.fn.pumvisible() > 0 then
+    return vim.api.nvim_replace_termcodes("<C-n>", true, true, true)
+  end
+
+  if check_back_space() then
+    return vim.api.nvim_replace_termcodes("<TAB>", true, true, true)
+  end
+
+  return vim.fn["coc#refresh"]()
+end
+
+vim.api.nvim_set_keymap("i", "<TAB>", "v:lua.tab_completion()", { expr = true, noremap = false })
