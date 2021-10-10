72a53b67f1d64e0e11db7b5d50e0abfe8c5845b5
LordMZTE <lord@mzte.de>
2021-10-11 00:48:23 +0200

    fix nvimtree config

--- .config/nvim/lua/pluginconf/nvimtree.lua
+++ .config/nvim/lua/pluginconf/nvimtree.lua
@@ -1,10 +1,12 @@
-require'nvim-tree'.setup {
+require 'nvim-tree'.setup {
     -- don't open tree when using firenvim
     open_on_setup = not vim.g.started_by_firenvim,
     auto_close = true,
-    lsp_diagnostics = true,
+    diagnostics = {
+        enable = true,
+    },
     view = {
         auto_resize = true,
-    }
+    },
 }
 
