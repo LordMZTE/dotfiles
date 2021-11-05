0f3c761d27874a9e17d2d03de3d61be74afbf5c7
LordMZTE <lord@mzte.de>
2021-11-05 12:28:21 +0100

    switch to i3status-rust

--- i3status/README.md
+++
@@ -1,3 +0,0 @@
-# i3status
-custom i3status replacement.
-to use with my config, build this and move it to `~/.config/i3/i3status`
--- i3status/src/json.rs
+++
@@ -1,7 +0,0 @@
-use serde::Serialize;
-#[derive(Debug, Serialize)]
-pub struct Block {
-    pub full_text: String,
-    // this is always a const color string, so this is fine for now
-    pub color: &'static str,
-}
