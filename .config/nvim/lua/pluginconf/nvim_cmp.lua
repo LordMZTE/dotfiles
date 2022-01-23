d9a0c946cd764c5705f3f22cb70272033e6d8f7f
LordMZTE <lord@mzte.de>
2022-01-23 14:16:44 +0100

    update cmp config with jump to completion fields

--- .config/nvim/lua/pluginconf/nvim_cmp.lua
+++ .config/nvim/lua/pluginconf/nvim_cmp.lua
@@ -1,4 +1,10 @@
 local cmp = require "cmp"
+local luasnip = require "luasnip"
+
+local function has_words_before()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 cmp.setup {
     snippet = {
@@ -11,7 +17,17 @@ cmp.setup {
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item()),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item()),
         ["<CR>"] = cmp.mapping.confirm { select = true },
     },
