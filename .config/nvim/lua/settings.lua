f82e6aaf4a03f1c3f149a07df01d5453d4036f79
LordMZTE <lord@mzte.de>
2022-01-25 23:35:43 +0100

    nvim: disable garbage providers

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -15,9 +15,15 @@ o.termguicolors = true
 wo.cursorline = true
 wo.cursorcolumn = true
 
-g.airline_powerline_fonts = 1
 g.neoterm_default_mod = "tab"
 g.neovide_iso_layout = true
+
+-- disable garbage providers
+g.loaded_python_provider = false;
+g.loaded_python3_provider = false;
+g.loaded_ruby_provider = false;
+g.loaded_perl_provider = false;
+g.loaded_node_provider = false;
 
 cmd "colorscheme dracula"
 
