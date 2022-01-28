941484cefc5450c52f743af96875c48d45ea3580
LordMZTE <lord@mzte.de>
2022-01-28 22:49:48 +0100

    format lua

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -19,11 +19,11 @@ g.neoterm_default_mod = "tab"
 g.neovide_iso_layout = true
 
 -- disable garbage providers
-g.loaded_python_provider = false;
-g.loaded_python3_provider = false;
-g.loaded_ruby_provider = false;
-g.loaded_perl_provider = false;
-g.loaded_node_provider = false;
+g.loaded_python_provider = false
+g.loaded_python3_provider = false
+g.loaded_ruby_provider = false
+g.loaded_perl_provider = false
+g.loaded_node_provider = false
 
 cmd "colorscheme dracula"
 
