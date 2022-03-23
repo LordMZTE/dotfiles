1ae7ea66ed73f89bef8def5a5ae2369c897d4204
LordMZTE <lord@mzte.de>
2022-03-23 12:33:52 +0100

    update conceals

---
+++ .config/nvim/queries/lua/highlights.scm
@@ -0,0 +1,11 @@
+;; Keywords
+(("else" @keyword) (#set! conceal ""))
+(("for" @keyword) (#set! conceal "ﳁ"))
+(("function" @keyword) (#set! conceal "λ"))
+(("if" @keyword) (#set! conceal ""))
+(("local" @keyword) (#set! conceal ""))
+(("return" @keyword) (#set! conceal ""))
+(("while" @keyword) (#set! conceal "ﯩ"))
+
+;; Functions
+((function_call name: (identifier) @function (#eq? @function "require")) (#set! conceal ""))
