aeacd9cdef5b4e1c0d4006c6e3e0683242dc191b
LordMZTE <lord@mzte.de>
2022-06-13 22:37:16 +0200

    add conceals for zig

---
+++ .config/nvim/queries/zig/highlights.scm
@@ -0,0 +1,32 @@
+;; Keywords
+(("async" @keyword) (#set! conceal "ﰦ"))
+(("const" @keyword) (#set! conceal ""))
+(("else" @keyword) (#set! conceal ""))
+(("enum" @keyword) (#set! conceal ""))
+(("fn" @keyword) (#set! conceal "λ"))
+(("for" @keyword) (#set! conceal "ﳁ"))
+(("if" @keyword) (#set! conceal ""))
+(("pub" @keyword) (#set! conceal ""))
+(("return" @keyword) (#set! conceal ""))
+(("struct" @keyword) (#set! conceal "📦"))
+(("switch" @keyword) (#set! conceal ""))
+(("var" @keyword) (#set! conceal ""))
+(("while" @keyword) (#set! conceal "ﯩ"))
+(("try" @keyword) (#set! conceal ""))
+(("comptime" @keyword) (#set! conceal "ﳼ"))
+
+;; Functions
+((BUILTINIDENTIFIER) @include
+  (#any-of? @include "@import" "@cImport")
+  (#set! conceal ""))
+
+;; Common Variables
+(((IDENTIFIER) @variable
+ (#eq? @variable "self"))
+    (#set! conceal ""))
+
+;; Operators
+(("&" @operator) (#set! conceal ""))
+(("*" @operator) (#set! conceal ""))
+(("=>" @operator) (#set! conceal "ﰲ"))
+(("?" @operator) (#set! conceal ""))
