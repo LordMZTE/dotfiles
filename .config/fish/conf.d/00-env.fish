fa279ad4422a2c873c2b7723b0e5dca087058774
LordMZTE <lord@mzte.de>
2022-08-15 13:38:14 +0200

    fix path priority

--- .config/fish/conf.d/00-env.fish
+++ .config/fish/conf.d/00-env.fish
@@ -6,5 +6,5 @@ export CXX=clang++
 export EDITOR=nvim
 
 # paths
-export PATH="$PATH:$HOME/.cargo/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin:$HOME/go/bin"
+export PATH="$HOME/.cargo/bin:$HOME/.local/bin:/var/lib/snapd/snap/bin:$HOME/go/bin:$PATH"
 export LUA_CPATH="$HOME/.local/lib/lua/?.so;$HOME/.local/lib/lua/?.lua;;"
