d20dc292bee8f00e76ae9b20235b0aa5eda89c78
LordMZTE <lord@mzte.de>
2022-09-09 20:49:16 +0200

    automatically strip mzteinit binary in release mode

--- scripts/mzteinit/build.zig
+++ scripts/mzteinit/build.zig
@@ -14,6 +14,7 @@ pub fn build(b: *std.build.Builder) void {
 
     const exe = b.addExecutable("mzteinit", "src/main.zig");
     exe.setTarget(target);
+    exe.strip = mode != .Debug;
 
     pkgs.addAllTo(exe);
 
