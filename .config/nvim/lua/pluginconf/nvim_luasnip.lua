be1036a8f31746131be601a9f199b5a3534769ec
LordMZTE <lord@mzte.de>
2022-10-18 15:32:49 +0200

    add shrug snippet

--- .config/nvim/lua/pluginconf/nvim_luasnip.lua
+++ .config/nvim/lua/pluginconf/nvim_luasnip.lua
@@ -1,2 +1,10 @@
+local ls = require "luasnip"
+
 require("luasnip.loaders.from_vscode").load()
 require("luasnip.loaders.from_snipmate").load()
+
+ls.add_snippets("markdown", {
+    ls.snippet("shrug", {
+        ls.text_node [[¯\\_(ツ)_/¯]],
+    }),
+})
