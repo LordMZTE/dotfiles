bab46a305a7b836dc5051e8f2895f32383a42237
LordMZTE <lord@mzte.de>
2022-11-14 23:02:59 +0100

    add ligs for java

---
+++ .config/nvim/queries/java/highlights.scm
@@ -0,0 +1,22 @@
+; extends
+
+;; Keywords
+(("abstract" @keyword) (#set! conceal ""))
+(("class" @keyword) (#set! conceal "📦"))
+(("enum" @keyword) (#set! conceal ""))
+(("extends" @keyword) (#set! conceal ""))
+(("final" @keyword) (#set! conceal ""))
+(("for" @keyword) (#set! conceal "ﳁ"))
+(("if" @keyword) (#set! conceal ""))
+(("implements" @keyword) (#set! conceal ""))
+(("import" @keyword) (#set! conceal ""))
+(("package" @keyword) (#set! conceal ""))
+(("public" @keyword) (#set! conceal ""))
+(("return" @keyword) (#set! conceal ""))
+(("switch" @keyword) (#set! conceal ""))
+(("synchronized" @keyword) (#set! conceal "ﰦ"))
+(("try" @keyword) (#set! conceal ""))
+(("while" @keyword) (#set! conceal "ﯩ"))
+
+;; Common Variables
+(((this) @variable.builtin) (#set! conceal ""))
