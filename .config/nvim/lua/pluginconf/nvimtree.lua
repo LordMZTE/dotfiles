2434c43f4daaf2b9d18fcd0513fa25de0c77de75
LordMZTE <lord@mzte.de>
2022-11-18 22:44:37 +0100

    update nvimtree config

--- .config/nvim/lua/pluginconf/nvimtree.lua
+++ .config/nvim/lua/pluginconf/nvimtree.lua
@@ -3,6 +3,9 @@ local map = vim.api.nvim_set_keymap
 require("nvim-tree").setup {
     open_on_setup = not vim.g.started_by_vinput,
     open_on_setup_file = false,
+    view = {
+        adaptive_size = true,
+    },
     diagnostics = {
         enable = true,
     },
@@ -11,6 +14,7 @@ require("nvim-tree").setup {
     },
     renderer = {
         indent_markers = { enable = true },
+        group_empty = true,
     },
 }
 
