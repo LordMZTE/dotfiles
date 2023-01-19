3761fcf510c6fb395fcde647689e77bb3d4c7cee
LordMZTE <lord@mzte.de>
2023-01-19 08:37:17 +0100

    update font names

--- .config/luakit/usersettings.lua
+++ .config/luakit/usersettings.lua
@@ -1,15 +1,17 @@
 local settings = require "settings"
 
+local font_family = "Iosevka Nerd Font"
+
 settings.application.prefer_dark_mode = true
 
 settings.webview.enable_accelerated_2d_canvas = true
 settings.webview.enable_webgl = true
 settings.webview.javascript_can_access_clipboard = true
 
-settings.webview.default_font_family = "Iosevka"
-settings.webview.monospace_font_family = "Iosevka"
-settings.webview.sans_serif_font_family = "Iosevka"
-settings.webview.serif_font_family = "Iosevka"
+settings.webview.default_font_family = font_family
+settings.webview.monospace_font_family = font_family
+settings.webview.sans_serif_font_family = font_family
+settings.webview.serif_font_family = font_family
 
 settings.window.default_search_engine = "duckduckgo"
 settings.window.home_page = "luakit://newtab"
--- mzte-nv/conf/ginit.vim
+++
@@ -1,6 +0,0 @@
-if exists("g:GtkGuiLoaded")
-    call rpcnotify(1, 'Gui', 'Font', 'Iosevka 12')
-else
-    GuiFont! Iosevka:h12
-endif
-
