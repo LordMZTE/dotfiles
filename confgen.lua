eb7513b2f66fd2b7d4d6e02c81db468512838e32
LordMZTE <lord@mzte.de>
2023-01-21 00:01:23 +0100

    add local conf feature to confgen.lua

--- confgen.lua
+++ confgen.lua
@@ -6,3 +6,15 @@ cg.addPath ".cargo"
 for k, v in pairs(require "cg_opts") do
     cg.opt[k] = v
 end
+
+-- This function is called in templates to allow adding device-specific configs.
+cg.opt.getDeviceConf = function(id)
+    local path = os.getenv "HOME" .. "/.config/mzte_localconf/" .. id
+    local file = io.open(path, "r")
+
+    if file == nil then
+        return ""
+    end
+
+    return file:read "*a"
+end
