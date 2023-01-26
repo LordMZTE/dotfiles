5fedc8a9d421354571d943dff9e4089a192af218
LordMZTE <lord@mzte.de>
2023-01-26 08:18:52 +0100

    improve terminal confgen stuff

--- cg_opts.lua
+++ cg_opts.lua
@@ -3,6 +3,11 @@ local opts = {}
 opts.font = "Iosevka Nerd Font"
 opts.term_font = "Iosevka Term Nerd Font Mono"
 
-opts.term_command = "wezterm"
+opts.term = {
+    name = "Wezterm",
+    command = "wezterm",
+    workdir_command = "wezterm-gui start --cwd",
+    icon_name = "org.wezfurlong.wezterm",
+}
 
 return opts
