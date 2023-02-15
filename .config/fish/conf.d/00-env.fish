497bb42565cfcdc0e76cb68eb281e58c14030cfa
LordMZTE <lord@mzte.de>
2023-02-15 18:39:54 +0100

    speed up fish env

--- .config/fish/conf.d/00-env.fish
+++ .config/fish/conf.d/00-env.fish
@@ -18,5 +18,5 @@ export PATH="$HOME/.mix/escripts:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/go/bin:
 export LUA_CPATH="$HOME/.local/lib/lua/?.so;$HOME/.local/lib/lua/?.lua;;"
 
 if which racket > /dev/null
-    set -ax PATH (racket -e '(require setup/dirs) (display (path->string (find-user-console-bin-dir)))')
+    set -ax PATH (racket -l racket/base -e '(require setup/dirs) (display (path->string (find-user-console-bin-dir)))')
 end
