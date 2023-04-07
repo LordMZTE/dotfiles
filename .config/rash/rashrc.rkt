7901a657e288e15f63aca9a9c6979e4265ea0739
LordMZTE <lord@mzte.de>
2023-04-07 15:38:29 +0200

    rash: add neovim alias

--- .config/rash/rashrc.rkt
+++ .config/rash/rashrc.rkt
@@ -4,9 +4,13 @@
   rash/private/rashrc-lib
   readline/pread)
 
+(provide nv)
+
 (current-prompt-function
   (λ (#:last-return-value retval)
     (unless (void? retval)
       (display ((current-rash-top-level-print-formatter) retval)))
 
     (readline-prompt #{prompt show (if (exn:fail? retval) 1 0) insert |> port->bytes})))
+
+(define-simple-pipeline-alias nv nvim)
