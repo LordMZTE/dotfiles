b6239e30e24b212ca670b9e64a2e8ffa48e4907d
LordMZTE <lord@mzte.de>
2024-03-25 21:28:48 +0100

    tampermonkey script: support site styles

--- plugins/tampermonkey-mzte/src/Macro.hx
+++ plugins/tampermonkey-mzte/src/Macro.hx
@@ -1,8 +1,11 @@
 #if macro
 import haxe.macro.Compiler;
 import haxe.macro.Context;
+import sys.FileSystem;
 import sys.io.File;
 #end
+
+using StringTools;
 
 macro function init() {
     var oldHeader = Context.definedValue("source-header");
@@ -15,3 +18,14 @@ macro function fileContent(path:String):haxe.macro.Expr.ExprOf<String> {
     return macro $v{Std.string(File.read(path).readAll())};
 }
 
+macro function siteStyles():haxe.macro.Expr.ExprOf<Map<String, String>> {
+    var map:Array<haxe.macro.Expr> = [];
+
+    for (f in FileSystem.readDirectory("assets/site_styles")) {
+        if (f.endsWith(".css")) {
+            map.push(macro $v{f.substr(0, f.length - 4)} => Macro.fileContent($v{'assets/site_styles/$f'}));
+        }
+    }
+
+    return macro $a{map};
+}
--- plugins/tampermonkey-mzte/src/Main.hx
+++ plugins/tampermonkey-mzte/src/Main.hx
@@ -1,6 +1,8 @@
 import js.Browser;
 
 function main() {
+    var siteStyles = Macro.siteStyles();
+
     var ownStyle = Browser.document.createStyleElement();
     ownStyle.innerHTML = Macro.fileContent("assets/own_style.css");
 
@@ -12,6 +14,14 @@ function main() {
 
         if (!actions.ToggleStyleAction.blacklist.has(Browser.window.location.hostname)) {
             Browser.document.body.appendChild(style);
+        }
+
+        var siteStyle = siteStyles[Browser.window.location.hostname];
+        if (siteStyle != null) {
+            var siteStyleElem = Browser.document.createStyleElement();
+            siteStyleElem.innerHTML = siteStyle;
+            Browser.document.body.appendChild(siteStyleElem);
+            Browser.console.log("added site style: ", siteStyleElem);
         }
     });
 
