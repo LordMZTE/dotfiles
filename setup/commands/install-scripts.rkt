a541d203bdb36892a6ec1bbbcad60a8bac022270
LordMZTE <lord@mzte.de>
2024-04-19 20:30:55 +0200

    remove redundant update-nvim-plugins script

--- setup/commands/install-scripts.rkt
+++ setup/commands/install-scripts.rkt
@@ -12,7 +12,6 @@
   (install-link "scripts/brightness.rkt" (bin-path "brightness"))
   (install-link "scripts/map-touch-display.rkt" (bin-path "map-touch-display"))
   (install-link "scripts/sysupdate.rkt" (bin-path "sysupdate"))
-  (install-link "scripts/update-nvim-plugins.rkt" (bin-path "update-nvim-plugins"))
   (install-link "scripts/use-country-mirrors.sh" (bin-path "use-country-mirrors"))
   (install-link "scripts/videos-duration.sh" (bin-path "videos-duration"))
 
