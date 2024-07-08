39615bf693fb209320b64a5b508f6a32975cb995
LordMZTE <lord@mzte.de>
2024-07-09 00:47:38 +0200

    update nushell plugins

--- nix/nushell-plugins.nix
+++ nix/nushell-plugins.nix
@@ -7,11 +7,11 @@
       name = "nu-plugin-dbus";
       src = pkgs.fetchCrate {
         pname = "nu_plugin_dbus";
-        version = "0.7.0";
-        hash = "sha256-Xw8/8ROem+9fTRwFxr4p1ZKFVdRnsUwFhmyu87OylKY=";
+        version = "0.8.0";
+        hash = "sha256-Ogc4iw0LIDoxyQnoTXzbNaQ6jHtkjmWja/a3TB1TZjk=";
       };
 
-      cargoSha256 = "sha256-Rg6YCPUPib2U9FHHgKrTPyyWblj9QEbiKmQom3MpXPU=";
+      cargoSha256 = "sha256-Q7ASOcS/d2YM02kaIwRzIegGcESU3mr/qRpxwv4KGHo=";
 
       nativeBuildInputs = with pkgs; [ pkg-config ];
       buildInputs = with pkgs; [ dbus ];
@@ -20,21 +20,21 @@
       name = "nu-plugin-formats";
       src = pkgs.fetchCrate {
         pname = "nu_plugin_formats";
-        version = "0.94.2";
-        hash = "sha256-MqE6iD8MjavdSIxGZFGtuuzjZFcwtW7upFBlwOR08+o=";
+        version = "0.95.0";
+        hash = "sha256-nwfLQxVzzUfBn7m1F669NThqzG9bAXlM/lCAVGDKY8o=";
       };
 
-      cargoSha256 = "sha256-7EBD4WtKHRX+34fao3oXOlb1UPpejxEL6Vagy9iH92I=";
+      cargoSha256 = "sha256-e8VcSJYH5P4LR1bFbmeiiF+fSfRlqTEzfnkPtUFmB2I=";
     })
     (pkgs.rustPlatform.buildRustPackage {
       name = "nu-plugin-polars";
       src = pkgs.fetchCrate {
         pname = "nu_plugin_polars";
-        version = "0.94.2";
-        hash = "sha256-rSwJImUi9k+MLPKaBxgV8UU1gTWoxCJHAtkUtYVvbyA=";
+        version = "0.95.0";
+        hash = "sha256-d2giFOByeX/lLc6k3e4cf/RodwV2yLjqeZD+cmVIwK8=";
       };
 
-      cargoSha256 = "sha256-llJGgqaoM1fJ9bdA0ohlf/TQqYOWjrl1RoONgSeTDHo=";
+      cargoSha256 = "sha256-e0r693pJJM3IjKjjwxvoIUi3y6avUI9m2qQ9rpSAtLU=";
 
       doCheck = false; # Needs OpenSSL, which build doesn't for some reason.
     })
