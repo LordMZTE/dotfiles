a032ca8c62c42ede9df15fa708bcd7418d9ada37
LordMZTE <lord@mzte.de>
2024-09-24 12:29:17 +0200

    nushell: update plugins and disable external command errors

--- nix/nushell-plugins.nix
+++ nix/nushell-plugins.nix
@@ -1,25 +1,32 @@
 { config, lib, pkgs, ... }:
 let
-  nu-ver = "0.97.1";
+  nu-ver = "0.98.0";
 in
 {
   options.nushell-plugins = lib.mkOption { };
 
   config.nushell-plugins = [
     (builtins.getFlake
-      "git+https://git.mzte.de/LordMZTE/nu-plugin-jobcontrol.git?rev=9ffb0e3b3d4415035b1406f3d2f981bd9ac09579"
+      "git+https://git.mzte.de/LordMZTE/nu-plugin-jobcontrol.git?rev=40127dfa0573e0cc6c273d291f5423aa5964d76a"
     ).outputs.packages.${pkgs.system}.default
 
     (pkgs.rustPlatform.buildRustPackage rec {
       name = "nu-plugin-dbus";
-      version = "0.10.0";
-      src = pkgs.fetchCrate {
-        inherit version;
-        pname = "nu_plugin_dbus";
-        hash = "sha256-PrZ8iZIqcxzrtAVo8GnYQdbnbNphpJvqmd51/4UBF60=";
+      version = "0.11.0";
+      #src = pkgs.fetchCrate {
+      #  inherit version;
+      #  pname = "nu_plugin_dbus";
+      #  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      #};
+      src = pkgs.fetchFromGitHub {
+        # Updated Fork
+        owner = "Canvis-Me";
+        repo = "nu_plugin_dbus";
+        rev = "a0f8eb54355e4fbf121f5f8a0a7e7d67b07e33bd";
+        hash = "sha256-CrTVLbD7Q/swDCxiWcqoxkB8X6ydfxhTAZjoT0SoB4I=";
       };
 
-      cargoHash = "sha256-0i5OxunUT1K3hP9n496SCXe24lry4ModkspGlphkmBI=";
+      cargoHash = "sha256-zyoZc+ItEiWMBeu8Ulbn2lAzFH2DeBbP7Rs+QLsBo+Y=";
 
       nativeBuildInputs = with pkgs; [ pkg-config ];
       buildInputs = with pkgs; [ dbus ];
@@ -30,10 +37,10 @@ in
       src = pkgs.fetchCrate {
         inherit version;
         pname = "nu_plugin_formats";
-        hash = "sha256-zXyaoEGlXTW0V2W5SRhaucOGG97iWyVsf+OXIEtcQZo=";
+        hash = "sha256-/RJLHFlgKbshNeEF8YHdthZWTnJ8p1M2Xb1AJ44VvGs=";
       };
 
-      cargoHash = "sha256-M5H8BGCKLKkfSz9qQ24qvhA81jzri/ZyWg8xs/iwri0=";
+      cargoHash = "sha256-FIqE8u8RBVhGUvssRGLhH7kNvEfQFkYLDujTVT4liNA=";
     })
     (pkgs.rustPlatform.buildRustPackage rec {
       name = "nu-plugin-polars";
@@ -41,10 +48,10 @@ in
       src = pkgs.fetchCrate {
         inherit version;
         pname = "nu_plugin_polars";
-        hash = "sha256-OQsQSXesLJyrWO/c4AfTmYcviUKpZNrL1BePa6wMcwk=";
+        hash = "sha256-qG7popu37L60+N0C6ayvvQKVfDaZiE5G9JXTK4unY/w=";
       };
 
-      cargoHash = "sha256-cqNGI40VjqAkpU/jTwXuHrZZ3wJxiDGVQVl2HTXmYqw=";
+      cargoHash = "sha256-TvE5qJl+TQRhk0Q08zO37bLoJkVpQ5i0DAcnGwzNWzM=";
 
       doCheck = false; # Needs OpenSSL, which build doesn't for some reason.
     })
@@ -54,10 +61,10 @@ in
       src = pkgs.fetchCrate {
         inherit version;
         pname = "nu_plugin_query";
-        hash = "sha256-6UQHxjWDp5ak4ouyru5K9VGt8JaIzArYgyJnqe5d0KA=";
+        hash = "sha256-ZY/rrahYg1gYjq1qsaQ34JQXd0PzWt3h5XqCMFaoanE=";
       };
 
-      cargoHash = "sha256-z2betxX5fgzPlr1+9/IZVSUyb/hjw+4C9DHYWUwoWRg=";
+      cargoHash = "sha256-ieFwg/Y/FuO2Rq6cjc6eyskV8/MzBWJVaFSGdwb53qA=";
 
       nativeBuildInputs = with pkgs; [ pkg-config ];
       buildInputs = with pkgs; [ openssl ];
