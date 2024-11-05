a20c3ea6bf69f85b7c093336481b577d64aa12d8
LordMZTE <lord@mzte.de>
2024-11-05 08:07:36 +0100

    typstwatch: don't open half written files

--- scripts/typstwatch.sh
+++ scripts/typstwatch.sh
@@ -4,6 +4,8 @@ trap 'kill $(jobs -p); rm "$tmpf"' EXIT
 
 typst watch "$1" "$tmpf" "${@:2}" &
 
-while [ ! -f "$tmpf" ]; do sleep 1; done
+ # Without this, zathura may try to open a half-written file. This is a hack, but I don't know of a
+ # better approach.
+sleep 1
 
 zathura "$tmpf"
