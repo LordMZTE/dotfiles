de8328ed709f6e6248fc43c2ccaa18e6511bfddc
LordMZTE <lord@mzte.de>
2024-12-26 16:06:59 +0100

    various config updates

--- nix/nushell-plugins.nix
+++ nix/nushell-plugins.nix
@@ -4,10 +4,26 @@
 
   config.nushell-plugins = {
     jobcontrol = (builtins.getFlake
-      "git+https://git.mzte.de/LordMZTE/nu-plugin-jobcontrol.git?rev=3a02910ff138691ecf3557722411e53261d900d4"
+      "git+https://git.mzte.de/LordMZTE/nu-plugin-jobcontrol.git?rev=742c21477bb930697536b1f35ccf9c7be84451b8"
     ).outputs.packages.${pkgs.system}.default;
 
-    inherit (pkgs.nushellPlugins) polars formats query dbus;
+    dbus = pkgs.rustPlatform.buildRustPackage {
+      name = "nu_plugin_dbus";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "LordMZTE";
+        repo = "nu_plugin_dbus";
+        rev = "baa52026c3e8e4c6296d5545fd26237287436dad";
+        hash = "sha256-Ga+1zFwS/v+3iKVEz7TFmJjyBW/gq6leHeyH2vjawto=";
+      };
+
+      cargoHash = "sha256-wSEHmVenWlp4VUkz4VBtGR3U3Sf1KlXNC5YOa8A1l1c=";
+
+      nativeBuildInputs = with pkgs; [ pkg-config ];
+      buildInputs = with pkgs; [ dbus ];
+    };
+
+    inherit (pkgs.nushellPlugins) polars formats query;
   };
 
   config.output.packages.nushell-plugins = pkgs.writeTextFile {
