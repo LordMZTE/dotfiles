5efc45b591b8f8941aaf3c5e198fb45ae2ecbc59
LordMZTE <lord@mzte.de>
2025-01-17 12:42:52 +0100

    tampermonkey-mzte: remove more AI crap from GitHub

--- plugins/tampermonkey-mzte/src/siteplugin/Github.hx
+++ plugins/tampermonkey-mzte/src/siteplugin/Github.hx
@@ -2,13 +2,16 @@ package siteplugin;
 
 import js.Browser;
 
+final REMOVE_CLASSES:Array<String> = ["copilotPreview__container", "AppHeader-CopilotChat"];
+
 class Github implements ISitePlugin {
     public function new() {}
 
-	public function onContentLoaded() {
-        final ai_bullshit = Browser.document.getElementsByClassName("copilotPreview__container");
-        for (elem in ai_bullshit) {
-            elem.remove();
+    public function onContentLoaded() {
+        for (clazz in REMOVE_CLASSES) {
+            for (elem in Browser.document.getElementsByClassName(clazz)) {
+                elem.remove();
+            }
         }
     }
 }
