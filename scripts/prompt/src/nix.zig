cfee865e41e704f50421e1af83655c8544afb26a
LordMZTE <lord@mzte.de>
2025-03-19 16:19:27 +0100

    prompt: better nix shell detection logic

--- scripts/prompt/src/nix.zig
+++ scripts/prompt/src/nix.zig
@@ -3,20 +3,17 @@ const std = @import("std");
 /// If we're likely in a Nix shell, return the name of that shell or "?" if it's unknown,
 /// null otherwise.
 pub fn findNixShellName() ?[]const u8 {
-    return if (isInNixShell()) std.posix.getenv("name") orelse "?" else null;
-}
+    // "Legacy" nix shell
+    if (std.posix.getenv("IN_NIX_SHELL")) |_| return std.posix.getenv("name") orelse "?";
 
-fn isInNixShell() bool {
-    if (std.posix.getenv("IN_NIX_SHELL")) |_| return true;
-
-    var path_iter = std.mem.splitScalar(
+    // nix3 shell
+    // There is no env var to directly detect this, but the path of packages are prepended to $PATH,
+    // so we check if the first entry starts with /nix/store
+    if (std.mem.startsWith(
         u8,
-        std.posix.getenv("PATH") orelse return false,
-        ':',
-    );
+        std.posix.getenv("PATH") orelse return null,
+        "/nix/store",
+    )) return "NIX3";
 
-    while (path_iter.next()) |p|
-        if (std.mem.startsWith(u8, p, "/nix/store")) return true;
-
-    return false;
+    return null;
 }
