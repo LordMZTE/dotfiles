8d2039b0046ef4c71625e827e52fa8f61b87ac25
LordMZTE <lord@mzte.de>
2025-08-06 16:10:27 +0200

    mpv: add keybind for catppuccin LUT

--- nix/cgnix/catppuccin-lut.nix
+++ nix/cgnix/catppuccin-lut.nix
@@ -5,19 +5,33 @@ let
       --palette catppuccin-mocha \
       --output $out
   '';
-  lut_convert = with pkgs.python3Packages; buildPythonPackage {
-    src = pkgs.fetchFromGitHub {
-      owner = "mikeboers";
-      repo = "LUT-Convert";
-      rev = "5ee067e9dbe515afac2d385eff61278bf7ead652";
-      hash = "sha256-LTfd3PZWAdhisYP5gIZOEQtQCre7Z8LRYZycGukKRu8=";
-    };
+  lut_convert =
+    let
+      python = pkgs.python3.withPackages (p: [ p.pillow ]);
+    in
+    pkgs.stdenvNoCC.mkDerivation {
+      pname = "lut-convert";
+      version = "0-unstable-2013-05-23";
 
-    propagatedBuildInputs = [ pillow ];
-  };
+      src = pkgs.fetchFromGitHub {
+        owner = "mikeboers";
+        repo = "LUT-Convert";
+        rev = "5ee067e9dbe515afac2d385eff61278bf7ead652";
+        hash = "sha256-LTfd3PZWAdhisYP5gIZOEQtQCre7Z8LRYZycGukKRu8=";
+      };
+
+      doBuild = false;
+
+      installPhase = ''
+        mkdir -p $out/bin
+        echo '#!${python}/bin/python3' >> $out/bin/hald_to_cube
+        cat $src/hald_to_cube.py >> $out/bin/hald_to_cube
+        chmod +x $out/bin/hald_to_cube
+      '';
+    };
 in
 {
-  cgnix.entries.catpuccin_lut = pkgs.runCommand "catppuccin-mocha.cube" { } ''
+  cgnix.entries.catppuccin_lut = pkgs.runCommand "catppuccin-mocha.cube" { } ''
     ${lut_convert}/bin/hald_to_cube "${png_lut}" $out
   '';
 }
