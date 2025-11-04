ae35352a10f47f25a83833d2ba16f93481484f75
LordMZTE <lord@mzte.de>
2025-11-04 13:19:11 +0100

    mzte-nv: load treesitter on startup

--- mzte-nv/haxe/init/Plugins.hx
+++ mzte-nv/haxe/init/Plugins.hx
@@ -20,6 +20,7 @@ class Plugins {
             new PCatppuccin(), // avoid flicker of default theme
             new LuaPlugin("inlay-hint"), // same reason as lspconf
             new LuaPlugin("lspconf"), // loaded on startup for LSP in files opened in command
+            new LuaPlugin("treesitter"), // enable TS in files opened in command
         ];
         this.deferredPlugins = [cast(new PNu(), IPlugin)].concat([
             "cmp",
@@ -37,7 +38,6 @@ class Plugins {
             "nvimtree",
             "recorder",
             "telescope",
-            "treesitter",
             "ts-textobjects",
             "tterm",
             "overseer",
