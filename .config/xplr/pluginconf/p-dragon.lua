b4d70a9f5fe17096a26a231b55594cf96638a120
LordMZTE <lord@mzte.de>
2025-11-15 13:30:00 +0100

    dragon.xplr: use more common binary name

--- .config/xplr/pluginconf/p-dragon.lua
+++ .config/xplr/pluginconf/p-dragon.lua
@@ -1,1 +1,5 @@
-require("dragon").setup {}
+require("dragon").setup {
+    -- Seems like people are unsure of what the canonical binary name for dragon is. The default is
+    -- "dragon", but it seems to be "dragon-drop" on NixOS and I recall it being that on Arch too.
+    bin = "dragon-drop",
+}
