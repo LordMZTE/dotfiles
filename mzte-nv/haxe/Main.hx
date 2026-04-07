31be409ecedba8caf0a20c19f4180102883a47d4
LordMZTE <lord@mzte.de>
2026-04-07 20:46:19 +0200

    mzte-nv: stupid haxe luv deadlock hack

--- mzte-nv/haxe/Main.hx
+++ mzte-nv/haxe/Main.hx
@@ -4,7 +4,17 @@ import lua.Lua;
 import init.Settings;
 import init.Plugins;
 
+@:native("_hx_luv")
+private class StupidLuvDeadlockHack {
+    // Some versions of the Haxe compiler don't implement the `lua-vanilla` flag correctly, still
+    // emitting code that starts a libuv loop, which will cause a deadlock in our case.
+    // We use this dumb hack to make Haxe's attempt at this ineffective.
+    public static var run:Void -> Void;
+}
+
 function main() {
+    StupidLuvDeadlockHack.run = () -> {};
+
     Settings.init();
     new Plugins().init();
     Lua.require("maps");
